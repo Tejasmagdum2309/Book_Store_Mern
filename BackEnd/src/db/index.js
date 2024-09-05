18f93c50731063682ffd60a6d1ecc1ea385b29af
Tejasmagdum2309 <magdumtej1008@gmail.com>
2024-09-05 11:35:59 +0530

    final commit

--- BackEnd/src/db/index.js
+++ BackEnd/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://magdumtej1008:1Demx2xHcfVvOThF@cluster0.4izm9.mongodb.net/tejDB`)
+        const connectionInstance = await mongoose.connect(process.env.DB);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
