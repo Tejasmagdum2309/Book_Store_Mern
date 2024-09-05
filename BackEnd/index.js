48334476c66bb608151e69f2d350b9def5130d30
Tejasmagdum2309 <magdumtej1008@gmail.com>
2024-09-05 12:18:01 +0530

    added

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -16,10 +16,10 @@ const port = process.env.PORT || 8000;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/FrontEnd/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "FrontEnd", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
