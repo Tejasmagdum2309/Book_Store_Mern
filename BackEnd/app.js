f1b4b2486f7056f4a2656a7364a3988c24d02dfc
Tejasmagdum2309 <magdumtej1008@gmail.com>
2024-09-06 10:49:28 +0530

    simple changes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,7 +10,10 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://your-frontend-domain.com',  // Adjust to your frontend domain
+    credentials: true,
+  }));
 app.use(cookieParser());
 
 
