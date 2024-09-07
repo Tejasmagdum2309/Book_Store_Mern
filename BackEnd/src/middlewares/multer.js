f330aa76d28c226fdaaad36187b2aa8ecf1d7f22
Tejasmagdum2309 <104526254+Tejasmagdum2309@users.noreply.github.com>
2024-09-07 13:55:28 +0530

    Update multer.js
    updarted file

--- BackEnd/src/middlewares/multer.js
+++ BackEnd/src/middlewares/multer.js
@@ -1,15 +1,18 @@
-import multer from "multer";
+import multer from 'multer';
+import path from 'path';
+
+// Define the storage path based on the environment
+const storagePath = process.env.UPLOAD_PATH || 'public/temp';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "../../public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, storagePath));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
 export const upload = multer({ 
-    storage, 
-})
+  storage, 
+});
